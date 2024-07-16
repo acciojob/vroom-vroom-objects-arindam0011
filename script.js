501976ef554df8e9d3de8d9d0ffb589479ace497
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-07-17 01:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Complete the js code
 function Car(make, model) {
-	this.make=make,
-	this.model=model
+	this.make=make;
+	this.model=model;
 }
 Car.prototype.getMakeModel=function(){
-	return `${this.Make} ${this.Model}`;
+	return `${this.make} ${this.model}`;
 }
 function SportsCar(make, model, topSpeed) {
 	Car.call(this, make, model);
-	this.topSpeed=topSpeed
+	this.topSpeed=topSpeed;
 }
 
 SportsCar.prototype=Object.create(Car.prototype);
