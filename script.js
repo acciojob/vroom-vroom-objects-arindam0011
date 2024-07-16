a227b5adf4c6de1a79afe5398e8456974200b430
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-07-17 01:38:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function SportsCar(make, model, topSpeed) {
 SportsCar.prototype=Object.create(Car.prototype);
 SportsCar.prototype.constructor=SportsCar;
 
-SportsCar.prototype.getTopSpeed=functuion(){
+SportsCar.prototype.getTopSpeed=function(){
 	return this.TopSpeed;
 }
 
